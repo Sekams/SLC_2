ec1bd46ff8d559bfcf04e7bd315be2a444a19b36
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2017-07-04 15:14:41 +0300

    Add unit tests for fizz_buzz function

---
+++ Day_2/FizzBuzz/test_fizz_buzz.py
@@ -0,0 +1,52 @@
+import unittest
+import fizz_buzz
+
+
+class TestFizzBuzz(unittest.TestCase):
+    """
+        Test Cases for the fizz_buzz function
+    """
+
+    # Testing if function returns 'Fizz' for input 3
+    def test_fizz_1(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(3), 'Fizz', msg='should return `Fizz` for number divisible by 3')
+
+    # Testing if function returns 'Fizz' for input 33
+    def test_fizz_2(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(33), 'Fizz', msg='should return `Fizz` for number divisible by 3')
+
+    # Testing if function returns 'Buzz' for input 5
+    def test_buzz_1(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(5), 'Buzz', msg='should return `Buzz` for number divisible by 5')
+
+    # Testing if function returns 'Buzz' for input 25
+    def test_buzz_2(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(25), 'Buzz', msg='should return `Buzz` for number divisible by 5')
+
+    # Testing if function returns 'FizzBuzz' for input 15
+    def test_fizz_buzz_1(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(15), 'FizzBuzz',
+                         msg='should return `FizzBuzz` for number divisible by 3 and 5')
+
+    # Testing if function returns 'FizzBuzz' for input 105
+    def test_fizz_buzz_2(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(105), 'FizzBuzz',
+                         msg='should return `FizzBuzz` for number divisible by 3 and 5')
+
+    # Testing if function returns 101 for input 101 that is neither divisible by 3 or 5
+    def test_indivisible_1(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(101), 101,
+                         msg='should return the number if its in divisible by neither 3 or 5')
+
+    # Testing if function returns 8 for input 8 that is neither divisible by 3 or 5
+    def test_indivisible_2(self):
+        self.assertEqual(fizz_buzz.fizz_buzz(8), 8,
+                         msg='should return the number if its in divisible by neither 3 or 5')
+
+    # Testing if function returns 'Invalid Argument' for invalid input
+    def test_invalid(self):
+        self.assertEqual(fizz_buzz.fizz_buzz("8"), 'Invalid Argument',
+                         msg="should return 'Invalid Argument' if its input is invalid")
+
+if __name__ == "__main__":
+    unittest.main()
